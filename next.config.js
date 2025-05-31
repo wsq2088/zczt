702dcc865e24f557e6abf46e29b17c1697fa72c8
wsq2088 <wsq2088@163.com>
2025-06-01 03:36:46 +0800

    Configure GitHub Pages deployment

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true
+  },
+  basePath: '/zczt',
+  assetPrefix: '/zczt'
+}
+
+module.exports = nextConfig 
